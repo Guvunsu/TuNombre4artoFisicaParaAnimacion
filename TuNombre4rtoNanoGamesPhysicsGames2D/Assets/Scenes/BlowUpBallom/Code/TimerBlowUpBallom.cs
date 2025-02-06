4c3bab464c9988fd8d51b58978baadf9d7b01078
Guvunsu <gavryk_fray_rojas@hotmail.com>
2025-02-05 22:31:14 -0600

    25184

---
+++ TuNombre4rtoNanoGamesPhysicsGames2D/Assets/Scenes/BlowUpBallom/Code/TimerBlowUpBallom.cs
@@ -0,0 +1,26 @@
+using TMPro;
+using UnityEngine;
+
+public class TimerBlowUpBallom : MonoBehaviour {
+
+    [SerializeField, HideInInspector] bool timerActiveBlowUpBallom;
+    [SerializeField] float timerBlowUpBallom;
+    [SerializeField] TMP_Text timeTxtBlowUpBallom;
+
+    void Start() {
+        timerActiveBlowUpBallom = true;
+        timerBlowUpBallom = 0f;
+    }
+    void Update() {
+        IncrementCronometerBlowUpBallom();
+    }
+    void IncrementCronometerBlowUpBallom() {
+        if (timerActiveBlowUpBallom == true) {
+            timerBlowUpBallom += Time.deltaTime;
+            timeTxtBlowUpBallom.text = timerBlowUpBallom.ToString("F2");
+        } else if (timerActiveBlowUpBallom == false) {
+            timerBlowUpBallom = 6.66f;
+        }
+    }
+}
+
