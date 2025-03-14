dd6bfdffb7b537ecbcf4b39a7142897dbbdd52e4
Guvunsu <gavryk_fray_rojas@hotmail.com>
2025-03-14 11:06:01 -0600

    dsafsf

---
+++ TuNombre4rtoNanoGamesPhysicsGames2D/Assets/Scenes/¡RUN! ovni ¡RUUN!/code/TimerRUNovniRUUUN.cs
@@ -0,0 +1,33 @@
+using TMPro;
+using UnityEngine;
+namespace Gavryk.Physics.BlackHole
+{
+    public class TimerRUNovniRUUUN : MonoBehaviour
+    {
+        [SerializeField, HideInInspector] bool timerActive;
+        [SerializeField] float timer;
+        [SerializeField] TMP_Text timeTxt;
+
+        void Start()
+        {
+            timerActive = true;
+            timer = 0f;
+        }
+        void Update()
+        {
+            IncrementCronometerRUNovniRUUUN();
+        }
+        void IncrementCronometerRUNovniRUUUN()
+        {
+            if (timerActive == true)
+            {
+                timer += Time.deltaTime;
+                timeTxt.text = timer.ToString("F2");
+            }
+            else if (timerActive == false)
+            {
+                timer = 6.66f;
+            }
+        }
+    }
+}
