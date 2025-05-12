6eae4e042c6ff1c3ca57898e56102b976e1cda55
Guvunsu <gavryk_fray_rojas@hotmail.com>
2025-05-12 08:58:02 -0600

    651651

--- TuNombre4rtoNanoGamesPhysicsGames2D/Assets/Scenes/Gotcha!,Catch it!/Code/GameManagerGotchaCatchItPinball.cs
+++ TuNombre4rtoNanoGamesPhysicsGames2D/Assets/Scenes/Gotcha!,Catch it!/Code/GameManagerGotchaCatchItPinball.cs
@@ -1,16 +1,104 @@
+using TMPro;
 using UnityEngine;
-
+using static Gavryk.Physics.BlackHole.MovementOVNI;
+using static InputPlayer;
+// agregar el sistema de puntos 
 public class GameManagerGotchaCatchItPinball : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    #region ENUM
+    public enum GameState
     {
-        
+        PLAYING,
+        WIN,
+        LOSE
+    }
+    public enum fsmPlayer
+    {
+        STOP_IT
     }
 
-    // Update is called once per frame
+    #endregion ENUM
+
+    #region Variables
+
+    [SerializeField] GameState gameFSM;
+    [SerializeField] fsmPlayer playerFSM;
+
+    //[SerializeField] GameObject panelWin;
+    //[SerializeField] GameObject panelLose;
+
+    [SerializeField] Transform basket8x;
+    [SerializeField] Transform basket4x;
+    [SerializeField] Transform basket2x;
+
+    [SerializeField] TMP_Text scoreUpdate;
+    [SerializeField] TMP_Text ballCount;
+
+    #endregion Variables
+
+    void Start()
+    {
+
+    }
+
     void Update()
     {
-        
+
     }
+
+    #region CanastaThings
+
+    public void MultipliquerBasket()
+    {
+
+    }
+
+    #endregion CanastaThings
+
+    #region Pelotas
+    public void BallCounts()
+    {
+
+    }
+
+    #endregion Pelotas
+
+    #region Score
+    public void UpdateScore()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+
+        UpdateScore();
+    }
+    #endregion Score
+
+    //#region Victory&LosePanel
+    //public void VictoryPanel()
+    //{
+    //    if (gameFSM != GameState.PLAYING)
+    //    {
+    //        return;
+    //    }
+    //    gameFSM = GameState.WIN;
+    //    panelWin.SetActive(true);
+    //    playerFSM = fsmPlayer.STOP_IT;
+    //    panelLose.SetActive(false);
+    //}
+    //public void LosePanel()
+    //{
+    //    if (gameFSM != GameState.PLAYING)
+    //    {
+    //        return;
+    //    }
+    //    gameFSM = GameState.LOSE;
+    //    panelLose.SetActive(true);
+    //    playerFSM = fsmPlayer.STOP_IT;
+    //    panelWin.SetActive(false);
+    //}
+
+    //#endregion Victory&LosePanel
 }
