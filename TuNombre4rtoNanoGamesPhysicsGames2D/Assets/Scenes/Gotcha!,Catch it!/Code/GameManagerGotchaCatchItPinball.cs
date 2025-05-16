b085a072c79b7985f704f8d519f7555bc0fdfcd5
Guvunsu <gavryk_fray_rojas@hotmail.com>
2025-05-16 02:44:10 -0600

    5248

--- TuNombre4rtoNanoGamesPhysicsGames2D/Assets/Scenes/Gotcha!,Catch it!/Code/GameManagerGotchaCatchItPinball.cs
+++ TuNombre4rtoNanoGamesPhysicsGames2D/Assets/Scenes/Gotcha!,Catch it!/Code/GameManagerGotchaCatchItPinball.cs
@@ -1,104 +1,58 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using static Gavryk.Physics.BlackHole.MovementOVNI;
 using static InputPlayer;
-// agregar el sistema de puntos 
-public class GameManagerGotchaCatchItPinball : MonoBehaviour
-{
-    #region ENUM
-    public enum GameState
-    {
-        PLAYING,
-        WIN,
-        LOSE
-    }
-    public enum fsmPlayer
-    {
-        STOP_IT
-    }
 
-    #endregion ENUM
+public class GameManagerGotchaCatchItPinball : MonoBehaviour {
 
     #region Variables
-
-    [SerializeField] GameState gameFSM;
-    [SerializeField] fsmPlayer playerFSM;
-
-    //[SerializeField] GameObject panelWin;
-    //[SerializeField] GameObject panelLose;
+    MoveGotchaCatchItPinball script_MoveGotchaCatchItPinball;
 
     [SerializeField] Transform basket8x;
     [SerializeField] Transform basket4x;
     [SerializeField] Transform basket2x;
 
-    [SerializeField] TMP_Text scoreUpdate;
-    [SerializeField] TMP_Text ballCount;
+    int scoreAmount = 0;
+    int amount;
+    [SerializeField] TextMeshProUGUI scoreUpdate;
+    [SerializeField] TextMeshProUGUI ballCount;
 
     #endregion Variables
 
-    void Start()
-    {
+    void Start() {
 
     }
 
-    void Update()
-    {
-
+    void Update() {
+        UpdateScore();
     }
-
+    public void FinishBallCycle() {
+        script_MoveGotchaCatchItPinball.StopTheGame();
+    }
     #region CanastaThings
 
-    public void MultipliquerBasket()
-    {
-
+    public void MultipliquerBasket(int points) {
+        scoreAmount += points;
+        UpdateScore();
+    }
+    public void MultipliquerObstacules(int points) {
+        scoreAmount += points;
+        UpdateScore();
     }
 
     #endregion CanastaThings
 
-    #region Pelotas
-    public void BallCounts()
-    {
-
-    }
-
-    #endregion Pelotas
-
     #region Score
-    public void UpdateScore()
-    {
-
+    public void UpdateScore() {
+        scoreUpdate.text = ("") + scoreAmount.ToString();
     }
-
-    public void OnTriggerEnter(Collider other)
-    {
-
+    public void AddPointsGotchaGatchaCatchIt() {
+        scoreAmount += amount;
         UpdateScore();
     }
+    public void UpdateBallCount(int count) {
+        ballCount.text = "Pelotas: " + count.ToString();
+    }
     #endregion Score
-
-    //#region Victory&LosePanel
-    //public void VictoryPanel()
-    //{
-    //    if (gameFSM != GameState.PLAYING)
-    //    {
-    //        return;
-    //    }
-    //    gameFSM = GameState.WIN;
-    //    panelWin.SetActive(true);
-    //    playerFSM = fsmPlayer.STOP_IT;
-    //    panelLose.SetActive(false);
-    //}
-    //public void LosePanel()
-    //{
-    //    if (gameFSM != GameState.PLAYING)
-    //    {
-    //        return;
-    //    }
-    //    gameFSM = GameState.LOSE;
-    //    panelLose.SetActive(true);
-    //    playerFSM = fsmPlayer.STOP_IT;
-    //    panelWin.SetActive(false);
-    //}
-
-    //#endregion Victory&LosePanel
 }
--- TuNombre4rtoNanoGamesPhysicsGames2D/Assets/Scenes/Gotcha!,Catch it!/Code/MoveGotchaCatchItPinball.cs
+++ TuNombre4rtoNanoGamesPhysicsGames2D/Assets/Scenes/Gotcha!,Catch it!/Code/MoveGotchaCatchItPinball.cs
@@ -1,16 +1,74 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class MoveGotchaCatchItPinball : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+public class MoveGotchaCatchItPinball : MonoBehaviour {
+    #region ENUM
+    public enum PlayerState {
+        PLAYING,
+        STOP_IT
+    }
+    #endregion ENUM
+    #region Variables
+    [Header("PlayerState")]
+    public PlayerState playerState_FSM;
+
+    GameManagerGotchaCatchItPinball script_GameManagerGotchaCatchItPinball;
+    [SerializeField] GameObject ballPrefab;
+    [SerializeField] Transform pointEjectBall;
+    bool canIStartTheShoot = true;
+    int ballsGame = 9;
+
+    float time = 0;
+
+    #endregion Variables
+    void Start() {
+        script_GameManagerGotchaCatchItPinball.UpdateBallCount(ballsGame);
+    }
+    void Update() {
+        switch (playerState_FSM) {
+            case PlayerState.PLAYING:
+                break;
+            case PlayerState.STOP_IT:
+                break;
+        }
+    }
+    #region ShootTheBall
+
+    public void ShootTheBallGame(InputAction.CallbackContext value) {
+        if (value.performed && canIStartTheShoot && ballsGame > 0) {
+            GameObject ballInstantiate = Instantiate(ballPrefab, pointEjectBall.position, Quaternion.identity);
+            ballsGame--;
+            canIStartTheShoot = true;
+            playerState_FSM = PlayerState.PLAYING;
+            script_GameManagerGotchaCatchItPinball.UpdateBallCount(ballsGame);
+        }
+    }
+    public void StopTheGame() {
+        playerState_FSM = PlayerState.STOP_IT;
+        canIStartTheShoot = false;
+        Time.timeScale = 0;
+    }
+    public int GetRemainingBallsGame() {
+        return ballsGame;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Canasta")) {
+            playerState_FSM = PlayerState.PLAYING;
+            if (other.name.Contains("2X")) script_GameManagerGotchaCatchItPinball.MultipliquerBasket(2);
+            else if (other.name.Contains("4X")) script_GameManagerGotchaCatchItPinball.MultipliquerBasket(4);
+            else if (other.name.Contains("8X")) script_GameManagerGotchaCatchItPinball.MultipliquerBasket(8);
+            script_GameManagerGotchaCatchItPinball.FinishBallCycle();
+            script_GameManagerGotchaCatchItPinball.UpdateScore();
+            Destroy(gameObject);
+        }
+        if (other.gameObject.CompareTag("Obstacule")) {
+            playerState_FSM = PlayerState.PLAYING;
+            if (other.name.Contains("ObstaculosRectangulo")) script_GameManagerGotchaCatchItPinball.MultipliquerObstacules(2);
+            else if (other.name.Contains("ObstaculosCubo")) script_GameManagerGotchaCatchItPinball.MultipliquerObstacules(4);
+            else if (other.name.Contains("ObstaculosPiramide")) script_GameManagerGotchaCatchItPinball.MultipliquerObstacules(8);
+        }
     }
+
+    #endregion ShootTheBall
 }
